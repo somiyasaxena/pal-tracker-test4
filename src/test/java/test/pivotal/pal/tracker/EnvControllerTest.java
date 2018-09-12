359694b943bf22ebbccea82df45ee54b49a3d1d2
Somiya Saxena <somiya.s@HCL.COM>
2018-09-12 17:49:46 +0530

    commiting test cases

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -11,10 +11,10 @@ public class EnvControllerTest {
     @Test
     public void getEnv() throws Exception {
         EnvController controller = new EnvController(
-            "8675",
-            "12G",
-            "34",
-            "123.sesame.street"
+                "8675",
+                "12G",
+                "34",
+                "123.sesame.street"
         );
 
         Map<String, String> env = controller.getEnv();
